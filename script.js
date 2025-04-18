9b05971e28764c2850cf78a7ae275dd007c1c2a7
Kimathi Sedegah <sedkim370@gmail.com>
2025-04-18 01:57:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     }
   });
 });
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
 
 // ===== Animate Elements on Scroll =====
 const animateOnScroll = () => {
